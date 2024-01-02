5274e0915c15ad6043d0adbb0705ee3bd7a26214
haandol <ldg55d@gmail.com>
2024-01-02 23:59:18 +0900

    fix: make tracer local

--- infra/lib/functions/index.py
+++ infra/lib/functions/index.py
@@ -4,15 +4,16 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.sampling import ALWAYS_ON
 
-tracer = trace.get_tracer(
-    __name__,
-    tracer_provider=TracerProvider(sampler=ALWAYS_ON),
-)
+tracer_provider = TracerProvider(sampler=ALWAYS_ON)
 
 
 def handler(event, context):
     print(json.dumps(event))
 
+    tracer = trace.get_tracer(
+        __name__,
+        tracer_provider=tracer_provider,
+    )
     with tracer.start_span("requests amazon") as span:
         url = "https://aws.amazon.com/"
         requests.get(url, { 'timeout': 1, })
