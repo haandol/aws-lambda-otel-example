7c6cef3811120090d4819109def816ec84b5dee2
haandol <ldg55d@gmail.com>
2024-01-02 23:54:13 +0900

    fix: cannot override defauult provider

--- infra/lib/functions/index.py
+++ infra/lib/functions/index.py
@@ -4,13 +4,15 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.sampling import ALWAYS_ON
 
-# set the sampler onto the global tracer provider
-trace.set_tracer_provider(TracerProvider(sampler=ALWAYS_ON))
+tracer = trace.get_tracer(
+    __name__,
+    tracer_provider=TracerProvider(sampler=ALWAYS_ON),
+)
+
 
 def handler(event, context):
     print(json.dumps(event))
 
-    tracer = trace.get_tracer(__name__)
     with tracer.start_span("requests amazon") as span:
         url = "https://aws.amazon.com/"
         requests.get(url, { 'timeout': 1, })
@@ -22,6 +24,16 @@ def handler(event, context):
             1 / 0
         except ZeroDivisionError as err:
             span.record_exception(err)
-            raise err
+            return {
+                "statusCode": 500,
+                "body": json.dumps({
+                    "message": "Internal Server Error",
+                }),
+            }
 
-    return "ok"
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "ok",
+        }),
+    }
