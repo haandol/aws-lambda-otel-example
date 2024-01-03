6aac11e50b5dc9196fe58da1770aaea5bbb03f57
haandol <ldg55d@gmail.com>
2024-01-03 11:39:51 +0900

    chores: use semconv

--- infra/lib/functions/index.py
+++ infra/lib/functions/index.py
@@ -1,41 +1,54 @@
 import json
 import requests
+import traceback
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.sampling import ALWAYS_ON
+from opentelemetry.semconv.trace import SpanAttributes
 
-tracer_provider = TracerProvider(sampler=ALWAYS_ON)
+
+tracer = trace.get_tracer(__name__)
 
 
 def handler(event, context):
     print(json.dumps(event))
 
-    tracer = trace.get_tracer(
-        __name__,
-        tracer_provider=tracer_provider,
-    )
-    with tracer.start_span("requests amazon") as span:
+    with tracer.start_as_current_span("requests amazon") as span:
         url = "https://aws.amazon.com/"
-        requests.get(url, { 'timeout': 1, })
-        span.set_attribute("http.method", "GET")
-        span.set_attribute("http.url", url)
+        requests.get(
+            url,
+            {
+                "timeout": 1,
+            },
+        )
+        span.set_attribute(SpanAttributes.HTTP_METHOD, "GET")
+        span.set_attribute(SpanAttributes.HTTP_URL, url)
 
-    with tracer.start_span("devide zero") as span:
+    with tracer.start_as_current_span("devide zero") as span:
         try:
             1 / 0
         except ZeroDivisionError as err:
-            span.record_exception(err)
-            span.set_status(trace.StatusCode.ERROR, "ZeroDivisionError")
+            span.set_status(trace.StatusCode.ERROR)
+            span.record_exception(
+                err,
+                {
+                    SpanAttributes.EXCEPTION_STACKTRACE: traceback.format_exc(),
+                    SpanAttributes.EXCEPTION_ESCAPED: True,
+                },
+            )
             return {
                 "statusCode": 500,
-                "body": json.dumps({
-                    "message": "Internal Server Error",
-                }),
+                "body": json.dumps(
+                    {
+                        "stacktrace": traceback.format_exc(),
+                        "message": "zero division error",
+                    }
+                ),
             }
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "ok",
-        }),
+        "body": json.dumps(
+            {
+                "message": "ok",
+            }
+        ),
     }
